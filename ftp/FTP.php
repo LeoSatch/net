cc16bca0f8ac9ac996813d9a313e7cf0bb777160
Andre <andre@rihani.fr>
2015-10-10 21:50:10 +0200

    added accessors and TUs

--- ftp/FTP.php
+++ ftp/FTP.php
@@ -1,5 +1,7 @@
 <?php
-namespace symacopee\net;
+namespace Symacopee\Net;
+
+use InvalidArgumentException;
 
 /**
  * Description of FTP
@@ -14,11 +16,98 @@ class FTP {
   protected static $_nTmout;
   protected static $_oFP;
   
-  public static function connect( $sHost, $nPort = 22, $sUser = 'anonymous', $sPass = null, $nTMOUT = 60 ) {
+  public static function connect( $sHost = null, $nPort = 22, $sUser = 'anonymous', $sPass = null, $nTMOUT = 60 ) {
+    if ( ! $sHost || is_numeric( $sHost ) || is_bool($sHost) || is_resource($sHost)) {
+     throw new InvalidArgumentException();
+    }
+    
+    
     static::$_sHost = $sHost;
     static::$_nPort = $nPort;
     static::$_sUser = $sUser;
     static::$_sPass = $sPass;
     static::$_nPort = $nPort;
+    static::$_nTmout = $nTMOUT;
+  }
+  
+  /**
+   * 
+   * @return string host provided
+   */
+  public static function getHost() {
+    return static::$_sHost;
+  }
+  
+  /**
+   * 
+   * @return string username if provided
+   */
+  public static function getUsername() {
+    return static::$_sUser;
+  }
+  
+  /**
+   * 
+   * @return string password provided
+   */
+  public static function getPassword() {
+    return static::$_sPass;
+  }
+  
+  /**
+   * 
+   * @return string port if provided
+   */
+  public static function getPort() {
+    return static::$_nPort;
+  }
+  
+  /**
+   * 
+   * @return string timeout if provided
+   */
+  public static function getTimeOut() {
+    return static::$_nTmout;
+  }
+  
+  
+  /**
+   * 
+   * @param type $sFile
+   */
+  public static function get( $sFile ) {
+    
+  }
+  
+  /**
+   * 
+   * @param type $sDir
+   */
+  public static function chdir( $sDir ) {
+    
+  }
+  
+  /**
+   * 
+   * @param type $sFile
+   * @param type $sDest
+   */
+  public static function put( $sFile, $sDest ) {
+    
+  }
+  
+  /**
+   * 
+   * @param type $sFile
+   */
+  public static function delete( $sFile ) {
+    
+  }
+  
+  /**
+   * 
+   */
+  public static function close() {
+    
   }
 }
