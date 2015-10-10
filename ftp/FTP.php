13b3833d3c0bb9bfff30e4d780ade591be814f10
satch <andre@rihani.fr>
2015-10-10 18:42:07 +0200

    init

---
+++ ftp/FTP.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace symacopee\net;
+
+/**
+ * Description of FTP
+ *
+ * @author satch
+ */
+class FTP {
+  //put your code here
+}
